361b55c38669982e2d73d13e361380e9a934f99d
SSBeron <sultan.sagatov87@gmail.com>
2023-02-15 14:04:53 -0500

    add help section with faq and contact

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,37 @@
-import { Routes, Route, Link } from "react-router-dom";
+import { 
+  Route,
+  RouterProvider,
+  createBrowserRouter,
+  createRoutesFromElements
+  } from "react-router-dom";
+
+
 import Home from "./pages/Home"
 import About from "./pages/About"
+import Faq from "./pages/help/Faq"
+import Contact from "./pages/help/Contact"
+
+
+import RootLayout from "./layouts/RootLayout";
+import HelpLayout from "./layouts/HelpLayout";
+
+
+const myRouter = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<RootLayout />}>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+        <Route path="help" element={<HelpLayout/>}>
+            <Route path="faq" element={<Faq />} />
+            <Route path="contact" element={<Contact />} />
+        </Route>
+      </Route>
+  )
+)
 
 function App() {
   return (
-      <main>
-      <Routes>
-        <Route path="/" element={<Home/>}/>
-        <Route path="about" element={<About/>}/>
-      </Routes>
-    </main>
+     <RouterProvider router={myRouter} />
   )
 }
 
